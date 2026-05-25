af47624f378f7455b2a659352037015b628e4e55
Brunda D R <brundadr315@gmail.com>
2026-05-25 23:02:03 +0530

    https://chat-app-frontend-beige-kappa.vercel.app/ -- added frontend url to backend.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { app, connectMongo } = require("./app")
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000"
+        origin: "https://chat-app-frontend-beige-kappa.vercel.app"
     },
 })
 
